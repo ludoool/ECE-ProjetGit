ed5f3822f0ce24645de452e84695c86b428d6f71
Thibault Seris <Thibault@MacBook-Air-de-Thibault.local>
2018-02-12 13:06:10 +0100

    Coucou jtm

---
+++ gogogo/gogogo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gogogo
+//
+//  Created by Thibault Seris on 12/02/2018.
+//  Copyright © 2018 Thibault Seris. All rights reserved.
+//
+
+#include <iostream>
+
+int main{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
